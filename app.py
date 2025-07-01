20a9a98eb7edd1f57c161d04bee2c73275323372
soha0206 <94105927+soha0206@users.noreply.github.com>
2025-07-01 18:22:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -437,4 +437,5 @@ def confirm_generate():
     return redirect(url_for('generate_dataset'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT not set
+    app.run(host='0.0.0.0', port=port, debug=True)
